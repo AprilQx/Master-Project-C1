442c5d3ce246d34aa2c727296654a5d7872fad55
AprilQx <xxq_010223@163.com>
2024-11-23 14:37:03 +0000

    fixe the bugs in making html

--- dual_autodiff/docs/conf.py
+++ dual_autodiff/docs/conf.py
@@ -1,25 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'Dual Number Automatic Differentiation'
+# -- Project information -----------------------------------------------------
+project = 'dual_autodiff'
 copyright = '2024, Xueqing Xu'
 author = 'Xueqing Xu'
 
-# Add any Sphinx extension module names here
+# -- General configuration ---------------------------------------------------
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
+    'sphinx.ext.mathjax',
+    'nbsphinx',
+    'IPython.sphinxext.ipython_console_highlighting'
 ]
 
-
-
-# The theme to use for HTML and HTML Help pages
-html_theme = 'sphinx_rtd_theme'
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# NBSphinx settings
+nbsphinx_allow_errors = True
+nbsphinx_execute = 'always'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
--- dual_autodiff/docs/index.rst
+++ dual_autodiff/docs/index.rst
@@ -1,28 +1,27 @@
 Dual Number Automatic Differentiation
-==================================
+===================================
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents
+   :caption: Contents:
 
    theory
-   tutorial
+   dual_autodiff.ipynb
 
 Quick Example
 ------------
 
 .. code-block:: python
 
-   from dual_autodiff import Dual
-   x = Dual(2.0, 1.0)
-   y = x * x 
-   print(f"Value: {y.real}")     # 4.0
-   print(f"Derivative: {y.dual}") # 4.0
+    from dual_autodiff import Dual
+    x = Dual(2.0, 1.0)
+    y = x * x 
+    print(f"Value: {y.real}")     # 4.0
+    print(f"Derivative: {y.dual}") # 4.0
 
 Installation
 ------------
 
 .. code-block:: bash
 
-   pip install -e .
-
+    pip install -e .
